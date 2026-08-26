7847b3c3b8138b16c330cbbccce714ce580b960e
mpereyraarguello <mpereyraarguello@gmail.com>
2026-08-26 12:10:41 -0300

    multer

--- Proyecto1_Back/proyecto/src/modules/common/files/files.controller.ts
+++ Proyecto1_Back/proyecto/src/modules/common/files/files.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FilesService } from './files.service';
-
+import * as Multer from 'multer';
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
