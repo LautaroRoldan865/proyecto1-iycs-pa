401544539aa123765549a0c24ac7b5a8a4b39db4
victoriapetry1 <140028201+victoriapetry1@users.noreply.github.com>
2026-09-14 11:22:36 -0300

    feat(superlinea): crear entidad, DTOs, repositorio, configurar módulo y relacion con Linea

--- Proyecto1_Back/proyecto/src/modules/gestion-productos/superlinea/dto/create-superlinea.dto.ts
+++ Proyecto1_Back/proyecto/src/modules/gestion-productos/superlinea/dto/create-superlinea.dto.ts
@@ -9,9 +9,7 @@ import {
   IsBoolean,
 } from 'class-validator';
 
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateLineaDto {
+export class CreateSuperLineaDto {
   @Transform(({ value }) => value.trim().toLowerCase())
   @IsString({ message: 'La denominación debe ser una cadena de texto.' }) // Valida que sea string
   @IsNotEmpty({ message: 'La denominación no puede estar vacía.' }) // Valida que no esté vacía
@@ -22,31 +20,11 @@ export class CreateLineaDto {
   denominacion: string;
 
   @IsOptional()
-  @IsInt()
-  stockMinimo?: number;
-
-  @IsBoolean()
-  utilizaStockMinimo: boolean;
-
-  @IsNotEmpty({ message: 'La superLineaId es obligatoria.' })
-  @IsInt()
-  superLineaId:number;
-
-  @IsOptional()
   @IsString()
   observacion?: string;
 
-  createdAt?: Date;
 
   @IsNotEmpty({ message: 'El usuarioCreatedId es obligatorio.' })
   @IsInt({ message: 'El usuarioCreatedId debe ser un número entero.' })
   usuarioCreatedId: number;
-
-  @ApiProperty({
-    example: null,
-    description: 'Fecha de eliminación (null si está activa)',
-    nullable: true,
-  })
-  @IsOptional()
-  deletedAt: string | null;
 }
