6b1b13a6140ef1c8dd2ad68d19f459a01ccbb6e5
Lautaro <150679167+LautaroRoldan865@users.noreply.github.com>
2026-09-16 07:48:18 -0300

    .

--- Proyecto1_Back/proyecto/src/modules/gestion-productos/producto/domain/entities/producto.entity.ts
+++ Proyecto1_Back/proyecto/src/modules/gestion-productos/producto/domain/entities/producto.entity.ts
@@ -154,13 +154,12 @@ export class Producto {
   @Column({ type: 'int', nullable: true })
   marcaId?: number;
 
-  /* ESTOS LOS SACARIAMOS PARA PODER REPRESENTARLOS EN PRESENTACION (VO)
   @Column({ default: false })
   utilizaPack: boolean;
 
   @Column({ type: 'int', nullable: true })
   cantidadPorPack: number | null;
-  */
+
 
   @Column({ type: 'text', nullable: true })
   imagen?: string;
