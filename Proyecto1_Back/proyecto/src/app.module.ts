fd4e8134715d95209bee698dbf4c1f388b135cd3
felipezandrino24 <felipezandrino80.2@gmail.com>
2026-09-20 20:06:21 -0300

    arreglo: agrega migración faltante de la entidad presentacion

--- Proyecto1_Back/proyecto/src/app.module.ts
+++ Proyecto1_Back/proyecto/src/app.module.ts
@@ -53,9 +53,7 @@ import { PresentacionModule } from './modules/gestion-productos/presentacion/pre
       synchronize: false,  
       //descomentar ssl: process.env.DB_SSL === 'true' | comentar para probarlo en local -mili
       //ssl: process.env.DB_SSL === 'true',
-      ssl: {
-        rejectUnauthorized: true,
-      },
+      ...(process.env.DB_SSL === 'true' ? { ssl: { rejectUnauthorized: false } } : {}),
     }),
 
     MarcaModule,
